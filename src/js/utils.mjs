cc39f14e9098073dd74c7e774deaf2511998cd88
beautycodez <angellorl_94@hotmail.com>
2024-06-16 16:26:53 -0500

    add product to local storage

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -11,7 +11,12 @@ export function getLocalStorage(key) {
 }
 // save data to local storage
 export function setLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
+  let products = getLocalStorage('so-cart') || [];
+  console.log(typeof products);
+  console.log(products);
+  products.push(data);
+
+  localStorage.setItem(key, JSON.stringify(products));
 }
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
