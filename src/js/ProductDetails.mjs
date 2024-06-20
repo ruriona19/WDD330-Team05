390d97e549bb32165f2ea94cd59a109911bfd08a
Moroni Motta <dareevil7@gmail.com>
2024-06-20 20:54:43 -0300

    w2 team activity

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,19 +1,39 @@
-//This script file will contain the code to dynamically produce the product detail pages.
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+
 export default class ProductDetails {
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.dataSource = dataSource;
+  }
 
-    constructor(productId, dataSource) {
-        this.productId = productId;
-        this.product = {};
-        this.dataSource = dataSource;
-    }
+  init() {
+    this.renderProductDetails();
+  }
 
-    init() {
+  addToCart(product) {
+    let cart = getLocalStorage("so-cart") || [];
+    cart.push(product);
+    setLocalStorage("so-cart", cart);
+  }
 
-    }
+  renderProductDetails() {
+    const pDescription = document.querySelector('.product__description');
+    const pName = document.getElementById('product_name');
+    const pClassification = document.getElementById('product_classification');
+    const pColor = document.querySelector('.product__color');
+    const pPrice = document.querySelector('.product-card__price');
+    const img = document.querySelector('#product_image');
+    const title = document.querySelector('title');
+    const btn = document.getElementById('addToCart');
 
-    addToCart() {
-
-    }
-
-    renderProductDetails() { }
+    pName.innerHTML = this.dataSource["Name"];
+    pDescription.innerHTML = this.dataSource["DescriptionHtmlSimple"];
+    pClassification.innerHTML = this.dataSource["NameWithoutBrand"];
+    pColor.innerHTML = this.dataSource["Colors"][0]["ColorName"];
+    pPrice.innerHTML = this.dataSource["FinalPrice"];
+    img.src = this.dataSource["Image"];
+    img.alt = this.dataSource["Name"];
+    btn.setAttribute('data-id', this.dataSource["Id"]);
+    title.innerHTML = "Sleep Outside | " + this.dataSource["Name"];
+  }
 }
--- src/js/product.js
+++ src/js/product.js
@@ -1,29 +1,21 @@
-import { setLocalStorage, getLocalStorage, getParams } from "./utils.mjs";
+import { getParams } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
-const dataSource = new ProductData("tents");
+document.addEventListener("DOMContentLoaded", async () => {
+  const dataSource = new ProductData("tents");
+  const productId = getParams('product');
+  const productData = await dataSource.findProductById(productId);
 
-const productId = getParams('product');
-console.log(productId);
+  const productDetails = new ProductDetails(productId, productData);
+  productDetails.init();
 
-const product = new ProductDetails(productId, dataSource);
-product.init();
-
-function addProductToCart(product) {
-  // Retrieve the current carts storred in an array from localStorage
-  let cart = getLocalStorage("so-cart") || [];
-  // Add the new product to the cart array
-  cart.push(product);
-  // Save the updated cart back to localStorage
-  setLocalStorage("so-cart", cart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+  document
+    .getElementById("addToCart")
+    .addEventListener("click", (e) => addToCartHandler(e, productDetails));
+  
+  async function addToCartHandler(e, productDetails) {
+    const product = productDetails.dataSource;
+    productDetails.addToCart(product);
+  }
+});
