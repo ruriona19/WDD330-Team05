9b44822937f552a9167e33f19257dfbbc8805f86
Roberto Uriona <roberto.uriona@jalasoft.com>
2024-06-22 22:51:21 -0400

    Remove Extra Products - Streach Activity Step 2

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,21 +3,22 @@ import getLocalStorage from "./utils.mjs";
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart") || [];
   if (!cartItems.length) {
-    document.querySelector(".product-list").innerHTML = "<p>Your cart is empty</p>";
+    document.querySelector(".product-list").innerHTML =
+      "<p>Your cart is empty</p>";
     return;
   }
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 
   const total = cartItems.reduce((acc, item) => acc + item.FinalPrice, 0);
-  document.querySelector(".cart-total").innerHTML = `<p>Total: $${total.toFixed(2)}</p>`;
-  
+  document.querySelector(".cart-total").innerHTML =
+    `<p>Total: $${total.toFixed(2)}</p>`;
 }
 
 const btn = document.querySelector("button");
-  btn.addEventListener("click", () => {
-    alert("Checkout in progress!");
-  });
+btn.addEventListener("click", () => {
+  alert("Checkout in progress!");
+});
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
