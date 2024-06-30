d71d7175f20ee5e13210ca58d8c8fbd632d0c15e
Moroni Motta <dareevil7@gmail.com>
2024-06-29 23:01:25 -0300

    index.html error fixed

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,12 @@
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
+
 import { loadHeaderFooter } from "./utils.mjs";
 import getBackpackItems from "./backpack.js";
 
 async function initialize() {
-  const dataSource = new ProductData("tents");
-  const pList = document.querySelector(".product-list");
-  const list = new ProductList("Tents", dataSource, pList);
-
   try {
     await loadHeaderFooter();
     getBackpackItems();
-    list.init();
+
   } catch (error) {
     /* eslint-disable no-console */
     console.error("Error loading header and footer:", error);
