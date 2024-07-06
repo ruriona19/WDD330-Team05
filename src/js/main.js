c25406a1c5d77420bcf815618ac6e71ed78ca6c7
beautycodez <angellorl_94@hotmail.com>
2024-07-06 17:23:18 -0500

    Welcome the user if it is the first time

--- src/js/main.js
+++ src/js/main.js
@@ -8,5 +8,19 @@ async function initialize() {
     console.error("Error loading header and footer:", error);
   }
 }
+function firstVisit() {
+  let counter = parseInt(localStorage.getItem("visit#"));
+  if (isNaN(counter) || counter === null || counter === 0){
+    const visit = document.querySelector(".visit");
+    visit.innerHTML = "Hello! thank you for visiting us for the first time, please sign in to get multiple benefits in our website!";
+    localStorage.setItem("visit#", 1);
+  } else {
+    const visit = document.querySelector(".visit");
+    visit.innerHTML = "Thank you for comming back";
+    counter = counter + 1;
+    localStorage.setItem("visit#", counter)
+  }
+}
 
 initialize();
+firstVisit();
